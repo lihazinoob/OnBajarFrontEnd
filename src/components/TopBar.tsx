3fb9067d216d7ccf634d671e2c7c5cb1ddf53188
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-06-15 10:15:04 +0600

    The top menu bar is working fine

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,11 +1,24 @@
+"use client";
+
+import React from "react";
+
 import Link from "next/link";
+
 import { LuSearch } from "react-icons/lu";
 import { LuHeart } from "react-icons/lu";
 import { MdOutlineShoppingCart } from "react-icons/md";
+import { FaArrowTurnDown } from "react-icons/fa6";
 
 export default function TopBar() {
+  //state for tracking when hovering over the shop menu item, the dropdown is open or not
+  const [isShopHovered, setIsShopHovered] = React.useState<boolean>(false);
+
   return (
     <>
+      {/* Semi-transparent Overlay */}
+      {isShopHovered && (
+        <div className="fixed inset-0 top-[72px] bg-black/10 z-10" />
+      )}
       <header className="w-full border-b shadow-sm bg-white ">
         <div className="flex items-center justify-between px-12 py-4">
           {/* Logo */}
@@ -14,22 +27,92 @@ export default function TopBar() {
           </Link>
 
           {/* Menu Items */}
-          <nav className="hidden md:flex gap-2 font-medium">
-            {[
-              { href: "/", label: "Home" },
-              { href: "/shop", label: "Shop" },
-              { href: "/new-in", label: "New Arrivals" },
-              { href: "/accessories", label: "Bestsellers" },
-              { href: "/sale", label: "Sale" },
-            ].map((item) => (
+          <nav className="hidden md:flex gap-2 font-medium items-center">
+            {/* Shop Menu */}
+            <div
+              className="relative"
+              onMouseEnter={() => setIsShopHovered(true)}
+              onMouseLeave={() => setIsShopHovered(false)}
+            >
+              <div className="flex items-center">
+                <Link
+                  href="/new-in"
+                  className="px-1 py-2 rounded-md hover:text-gray-700 transition-colors duration-200 ease-in-out"
+                >
+                  Shop
+                </Link>
+                <FaArrowTurnDown className="mt-1" />
+              </div>
+              {/* Dropdown Menu */}
+              {isShopHovered && (
+                <div className="absolute top-8 left-0 mt-2 w-30 bg-gray-50 border border-gray-200 rounded-md shadow-lg z-30 px-4 py-2">
+                  <div className="flex flex-col items-center justify-center space-y-2 text-gray-500">
+                    <Link
+                      href="/category/jersey"
+                      className="hover:text-gray-700 transition-colors duration-200"
+                    >
+                      Jersey
+                    </Link>
+                    <Link
+                      href="/category/t-shirt"
+                      className="hover:text-gray-700 transition-colors duration-200"
+                    >
+                      T-shirt
+                    </Link>
+                    <Link
+                      href="/category/trousers"
+                      className="hover:text-gray-700 transition-colors duration-200"
+                    >
+                      Trousers
+                    </Link>
+                    <Link
+                      href="/category/shirt"
+                      className="hover:text-gray-700 transition-colors duration-200"
+                    >
+                      Shirt
+                    </Link>
+                    <Link
+                      href="/category/shorts"
+                      className="hover:text-gray-700 transition-colors duration-200"
+                    >
+                      Shorts
+                    </Link>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div>
               <Link
-                key={item.href}
-                href={item.href}
+                href="/new-in"
                 className="px-3 py-2 rounded-md hover:text-gray-700 transition-colors duration-200 ease-in-out"
               >
-                {item.label}
+                New Arrivals
               </Link>
-            ))}
+            </div>
+            <div>
+              <Link
+                href="/new-in"
+                className="px-3 py-2 rounded-md hover:text-gray-700 transition-colors duration-200 ease-in-out"
+              >
+                New Arrivals
+              </Link>
+            </div>
+            <div>
+              <Link
+                href="/accessories"
+                className="px-3 py-2 rounded-md hover:text-gray-700 transition-colors duration-200 ease-in-out"
+              >
+                Bestsellers
+              </Link>
+            </div>
+            <div>
+              <Link
+                href="/sale"
+                className="px-3 py-2 rounded-md hover:text-gray-700 transition-colors duration-200 ease-in-out"
+              >
+                Sale
+              </Link>
+            </div>
           </nav>
 
           {/* Icons */}
